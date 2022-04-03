19bcf53d4a510376a2935a2e0b83cfaaecf3c48e
pandas-a11y <0xliza.morozova@gmail.com>
2022-04-03 16:46:06 +0300

    Added and tested renamed 'comparators' to compare_aux, added and tested pkg_compare_diff in compare
    Added and tested pkg_compare_ver

--- compare_alt_branches/compare_aux.py
+++ compare_alt_branches/compare_aux.py
@@ -1,3 +1,5 @@
+import pkg_resources as p_res
+
 def pkg_compare_diff(dict_has_pkg, dict_lacks_pkg):
     """
     Outputs unmatched packages that are present in the 1st package dictionary,
@@ -15,3 +17,24 @@ def pkg_compare_diff(dict_has_pkg, dict_lacks_pkg):
     unmatched_pkg = frozenset(set_has_pkg - set_lacks_pkg)
     pkg_difference = {k: dict_has_pkg[k] for k in unmatched_pkg}
     return pkg_difference
+
+
+def pkg_compare_ver(pkg_dict_sis, pkg_dict_p10):
+    """
+    Outputs a dictionary of packages for which versions are higher in the 'sisyphus' branch.
+
+    :param pkg_dict_sis: Sisyphus package dictionary
+    :type pkg_dict_sis: dict
+    :param pkg_dict_p10: Compared package dictionary (p10 by default)
+    :type pkg_dict_p10: dict
+    :return: Dictionary of packages with higher version in 'sisyphus'
+    :rtype: dict
+    """
+    set_sis = {*pkg_dict_sis.keys()}
+    set_p10 = {*pkg_dict_p10.keys()}
+    pkg_in_both = frozenset(set_sis & set_p10)
+    higher_v_sis = dict()
+    for p in pkg_in_both:
+        if p_res.parse_version(pkg_dict_sis[p]) > p_res.parse_version(pkg_dict_p10[p]):
+            higher_v_sis[p] = pkg_dict_sis[p]
+    return higher_v_sis
--- tests/test_compare_aux.py
+++ tests/test_compare_aux.py
@@ -1,24 +1,35 @@
 from compare_alt_branches import compare_aux
 
+test_data_1 = {
+    ('a1', 'b1'): '0.1.0',
+    ('a1', 'b2'): '2.0.3',
+    ('a2', 'b2'): '0.4',
+    ('f5', 'f5'): '5.04',
+    ('b1', 'a1'): '1.0',
+    ('a7', 'a7'): '9.5'
+}
+
+test_data_2 = {
+    ('a1', 'b1'): '0.1',
+    ('a1', 'b3'): '2.14.158',
+    ('c2', 'b2'): '22.22',
+    ('f5', 'f5'): '5.40'
+}
+
+test_data_3 = {('a7', 'a7'): '10.0'}
+
 def test_pkg_compare_diff():
-    test_data_1 = {
-        ('a1', 'b1'): '1',
-        ('a1', 'b2'): '2',
-        ('a2', 'b2'): '2',
-        ('f5', 'f5'): '5',
-        ('b1', 'a1'): '1'
-    }
-
-    test_data_2 = {
-        ('a1', 'b1'): '1',
-        ('a1', 'b3'): '2',
-        ('c2', 'b2'): '2',
-        ('f5', 'f5'): '5'
-    }
-
     assert compare_aux.pkg_compare_diff(test_data_1, test_data_1) == {}
-    assert compare_aux.pkg_compare_diff(test_data_1, test_data_2) == {('a1', 'b2'): '2',
-                                                                      ('a2', 'b2'): '2',
-                                                                      ('b1', 'a1'): '1'}
-    assert compare_aux.pkg_compare_diff(test_data_2, test_data_1) == {('a1', 'b3'): '2',
-                                                                      ('c2', 'b2'): '2'}
+    assert compare_aux.pkg_compare_diff(test_data_1, test_data_2) == {('a1', 'b2'): '2.0.3',
+                                                                      ('a2', 'b2'): '0.4',
+                                                                      ('b1', 'a1'): '1.0',
+                                                                      ('a7', 'a7'): '9.5'}
+    assert compare_aux.pkg_compare_diff(test_data_2, test_data_1) == {('a1', 'b3'): '2.14.158',
+                                                                      ('c2', 'b2'): '22.22'}
+
+def test_pkg_compare_ver():
+    assert compare_aux.pkg_compare_ver(test_data_1, test_data_1) == {}
+    assert compare_aux.pkg_compare_ver(test_data_1, test_data_3) == {}
+    assert compare_aux.pkg_compare_ver(test_data_3, test_data_1) == test_data_3
+    assert compare_aux.pkg_compare_ver(test_data_1, test_data_2) == {}
+    assert compare_aux.pkg_compare_ver(test_data_2, test_data_1) == {('f5', 'f5'): '5.40'}
